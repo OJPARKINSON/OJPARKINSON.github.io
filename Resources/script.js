b156a35a82932aa0bd7f3fc397fef2105fb1c244
OJPARKINSON <olliepar01@gmail.com>
2018-12-14 17:52:06 +0000

    pre load screen and menu added

---
+++ Resources/script.js
@@ -0,0 +1,12 @@
+setTimeout(loadHome, 1000);
+function loadHome() {
+    document.getElementById("preload").style.display = "none";
+    document.getElementById("home").style.display = "block";
+}
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
+    
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
