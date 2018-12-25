0ab0ea27212844365de25e277881669cd704e3f4
OJPARKINSON <olliepar01@gmail.com>
2018-12-25 06:01:04 +0000

    desgin on final stright

--- Resources/script.js
+++ Resources/script.js
@@ -4,3 +4,17 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.display = "none";
 }
+
+
+
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var customSpace = document.getElementById("customSpace");
+var custom = customSpace.offsetTop;
+function myFunction() {
+    if (window.pageYOffset >= custom) {
+        navbar.classList.add("onScroll")
+    } else {
+        navbar.classList.remove("onScroll");
+    }
+}
